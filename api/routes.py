bb9c4c9cbc7a8251ce84e82ea0db2842ea796ad4
Roi Lopez Barata <137203489+RoiCorporation@users.noreply.github.com>
2026-03-30 10:34:34 +0200

    Replaced underscores for hyphens in the API endpoint URL.

--- api/routes.py
+++ api/routes.py
@@ -6,7 +6,7 @@ from api.models import EnvironmentalReadings
 router = APIRouter(prefix="/api", tags=["api"])
 
 
-@router.post("/insert_station_readings/")
+@router.post("/insert-station-readings/")
 def insert_station_readings(readings: EnvironmentalReadings, session: SessionDep):
     session.add(readings)
     session.commit()
